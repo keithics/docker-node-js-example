3cce45f3bc0ff47958e99cd6f8294d9df198fb07
keithics <keith@webninjamobile.com>
2021-02-20 03:50:46 +0800

    fix typo in env

--- server/config/index.ts
+++ server/config/index.ts
@@ -3,7 +3,7 @@
  */
 export const config = {
     MONGO_URL : process.env.MONGO_URL || '',
-    DEBUG_MONGO : process.env.MONGO_DEBUG || true,
+    DEBUG_MONGO : process.env.MONGO_DEBUG || false,
     PORT: process.env.MONGO_PORT || 2017,
     NODE_HOST : process.env.NODE_HOST || 'localhost',
     NODE_PORT : process.env.NODE_PORT || 3000
