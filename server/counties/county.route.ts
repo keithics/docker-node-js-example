d429b34b04a197cbaefcadd638fbebb7b8aa6a1c
keithics <keith@webninjamobile.com>
2021-02-20 04:55:47 +0800

    updated documentation

--- server/counties/county.route.ts
+++ server/counties/county.route.ts
@@ -5,12 +5,12 @@ export class IndexRoutes {
   public usCounties: USCounties = new USCounties()
 
   public routes(app): void {
-    /*
+    /**
       Seeds the database
      */
     app.route('/seed').get(this.usCounties.seed.bind(this.usCounties))
 
-    /*
+    /**
     Search county by key * required
      */
     app.route('/suggest').get(
